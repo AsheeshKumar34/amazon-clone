5c9ff592eb72606f2c1bad4e486dc007742fe7d0
AsheeshKumar34 <asheesh.kumar3496@gmail.com>
2020-10-21 22:39:28 +0530

    Firebase and react-router setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <p>Amazon Clone</p>
-    </div>
-  )
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <p>Checkout Page</p>
+          </Route>
+          <Route path="/login">
+            <p>Login Page</p>
+          </Route>
+          <Route path="/">
+            <p>Home page</p>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
